eeb9bf7b3dee773b74e3a33468ac1d16372b3c98
FMB237 <miguelfouenanf@gmail.com>
2026-04-27 22:17:02 +0100

    Adding Dashboard redirection system ,login sessions,logout page,Password_hash and middlewares

--- Readme.md
+++ Readme.md
@@ -19,3 +19,12 @@ The will be find inside the requirement.txt file
 
 Let add a real db and models.py for this work 
 
+Let move on to step 8 Setting up Security and Session
+So we will add : 
+- Password hashing
+- Session(Login stay in )
+- Redirect in dashboard
+Let install the necessary packets for that 
+Using the command **pip install passlib[bcrypt] itsdangerous**
+1. Let add password hash which is generally added into a file called auth.py for here we will just import it to our main.py file
+2. 
--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI,Request,HTTPException,Depends,status,Form
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse,RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from models import User
 from database import Base,engine,SessionLocal
+from passlib.context import  CryptContext
+from starlette.middleware.sessions import SessionMiddleware
 import uvicorn
 
 
@@ -13,6 +15,29 @@ templates= Jinja2Templates(directory='./templates')
 
 Base.metadata.create_all(bind=engine)
 
+# The middleware in quetion
+
+app.add_middleware(
+    SessionMiddleware,
+    secret_key="941f8c9cf212dbecdc17792b8db8c2246f988cd0a38b9f6ac8d0097be2eb3bf2"
+)
+
+
+# Let add some security to our code
+pwd_context = CryptContext(schemes=["bcrypt"],deprecated="auto")
+
+
+
+# Now add the helpfull auth functions
+
+def hash_password(password:str):
+    return pwd_context.hash(password[:72])
+
+def verify_password(plain,hashed):
+    return pwd_context.verify(plain,hashed)
+
+# Let add a session using middlewares
+
 
 @app.get('/',response_class=HTMLResponse)
 def home(request : Request):
@@ -21,14 +46,15 @@ def home(request : Request):
         name="login.html"
     )
 # Let now add a post request for our Form
-
+# Also update the login 
 @app.post('/login',response_class=HTMLResponse)
 def login(request:Request,username : str = Form(...) , password: str = Form(...)):
     db= SessionLocal()
     user = db.query(User).filter(User.username == username).first()
-    if user and user.password == password:
-         return "<p style='color:green;'>Login successful</p>"
-         
+    if user and verify_password(password,user.password):
+         request.session["user"] = user.username
+         return HTMLResponse(
+            '<script>window.location.href="/dashboard"</script>') # fOR Redirection to dashboard after login
     return "<p style='color:red;'>Invalid credentials</p>"
 
 
@@ -41,17 +67,38 @@ def get_SignUp(request:Request):
     )
 
 # Let add our Signup route for post requests
+# Update SignUp with new password rules
 @app.post('/SignUp',response_class=HTMLResponse)
-def SignUp(request:Request,username : str = Form(...),email:str = Form(...),password: str = Form(...,)):
+def Post_SignUp(request:Request,username : str = Form(...),email:str = Form(...),password: str = Form(...,)):
     db =SessionLocal() # Now let check if user exists ??
     existing_user = db.query(User).filter(User.username == username).first()
 
     if existing_user:
          return "<p style='color:red;'>User already exists</p>"
-    new_user = User(username=username,email=email,password=password)
+    # new password variable
+    hashed_pw = hash_password(password)
+    new_user = User(username=username,email=email,password=hashed_pw)
     db.add(new_user)
     db.commit()
     return "<p style='color:green;'>Registration successful</p>"
 
+
+# let add our dashboard route
+@app.get("/dashboard",response_class=HTMLResponse)
+def get_dashboard(request:Request):
+    user = request.session.get("user")
+    if not user:
+        return RedirectResponse(url="/")
+    return templates.TemplateResponse(
+        request=request,
+        name="dashboard.html"
+    )    
+
+
+# Let add logout
+@app.get("/logout")
+def logout(request:Request):
+    request.session.clear()
+    return RedirectResponse(url="/")
 if __name__=="__main__":
     uvicorn.run('main:app',host="0.0.0.0",port=8001,reload=True)
